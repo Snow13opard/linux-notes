540b82e26378b7cd4f30434eda509cce3a36d39e
Cody S. <codybricesands@gmail.com>
2015-10-03 00:11:23 -0700

    Update dropboxinstallscript.sh

--- dropboxinstallscript.sh
+++ dropboxinstallscript.sh
@@ -3,7 +3,6 @@ wget -O /sbin/dropbox.py https://www.dropbox.com/download?dl=packages/dropbox.py
 chmod +x /sbin/dropbox.py
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 
-
 read -p "Are you sure? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
