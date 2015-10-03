1382131fb7fa2124b1538dfe131919eb20cbcdcf
Cody S. <codybricesands@gmail.com>
2015-10-02 23:57:33 -0700

    Update dropboxinstallscript.sh

--- dropboxinstallscript.sh
+++ dropboxinstallscript.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+wget -O /sbin/dropbox.py https://www.dropbox.com/download?dl=packages/dropbox.py
+chmod +x /sbin/dropbox.py
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 
 echo "Do you wish to install this program?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) ~/.dropbox-dist/dropboxd; break;;
+        Yes ) echo "dropbox.py restart" | at now + 3 minute && ~/.dropbox-dist/dropboxd; break;;
         No ) exit;;
     esac
 done
