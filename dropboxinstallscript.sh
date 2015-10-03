3c9626af90a7ad6cb0e5ea9b3048286ff79aef64
Cody S. <codybricesands@gmail.com>
2015-10-03 00:20:19 -0700

    Update dropboxinstallscript.sh

--- dropboxinstallscript.sh
+++ dropboxinstallscript.sh
@@ -7,6 +7,6 @@ read -p "Are you sure? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
-    ~/.dropbox-dist/dropboxd & dropbox.py restart
+    ~/.dropbox-dist/dropboxd && dropbox.py restart
 fi
 exit
