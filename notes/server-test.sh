9e8558f9fc3ecfe99d0a76acfe813548624e18a7
Cody S. <codybricesands@gmail.com>
2015-10-05 05:31:26 -0700

    Delete server-test.sh

--- notes/server-test.sh
+++
@@ -1,7 +0,0 @@
-if curl -s https://raw.githubusercontent.com/Snow13opard/linux-notes/master/test.html  | grep -q 'iso777'; then
-echo "test worked"
-else
-echo "test failed"
-echo "uh oh web server went offline!" > msg.txt
-ssmtp recipient_name@gmail.com < msg.txt
-fi
